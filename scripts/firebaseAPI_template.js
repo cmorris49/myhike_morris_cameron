e9ddf397fdb51b2567564b4cd8701c5111bb449a
Cameron Morris <cameronwmorris49@gmail.com>
2025-02-18 12:39:07 -0800

    Demo07

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,19 +1,1 @@
-//----------------------------------------
-//  Your web app's Firebase configuration
-//----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
-
-//--------------------------------------------
-// initialize the Firebase app
-// initialize Firestore database if using it
-//--------------------------------------------
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 
