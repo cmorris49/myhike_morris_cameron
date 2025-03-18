13c0486f184d60e3ee6fb3c2fbc32e3791daf666
Cameron Morris <cameronwmorris49@gmail.com>
2025-03-18 09:43:11 -0700

    Demo 11 done

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,18 @@ function sayHello() {
     //do something
 }
 //sayHello();    //invoke function
+
+//----------------------------------------------------------
+// Wouldn't it be nice to see the User's Name on this page?
+// Let's do it!  (Thinking ahead:  This function can be carved out, 
+// and put into script.js for other pages to use as well).
+//----------------------------------------------------------//----------------------------------------------------------
+function insertNameFromFirestore(user) {
+  db.collection("users").doc(user.uid).get().then(userDoc => {
+      console.log(userDoc.data().name)
+      userName = userDoc.data().name;
+      console.log(userName)
+      document.getElementById("name-goes-here").innerHTML = userName;
+  })
+
+}
